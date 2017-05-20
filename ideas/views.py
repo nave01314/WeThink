e03f15adc8c55d62defe41bb04ded6512f138ce9
Evan Weissburg <nave01314@gmail.com>
2017-05-20 05:52:40 -0700

    Add html templates

--- ideas/views.py
+++ ideas/views.py
@@ -1,8 +1,28 @@
 from django.shortcuts import render
 
 from django.http import HttpResponse
+from django.http import Http404
+from django.template import loader
+
+from .models import Idea
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_idea_list = Idea.objects.order_by('-pub_date')[:5]
+    context = {
+        'latest_idea_list': latest_idea_list,
+    }
+    return HttpResponse(render(request, 'ideas/index.html', context))
+
+
+def detail(request, idea_id):
+    try:
+        idea = Idea.objects.get(id=idea_id)
+    except Idea.DoesNotExist:
+        raise Http404("Idea does not exist")
+
+    context = {
+        'idea': idea,
+    }
+    return HttpResponse(render(request, 'ideas/detail.html', context))
 
