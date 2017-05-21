5b5bf28efe87de6b54fd0a8278f1a777b7e0ad06
Evan Weissburg <nave01314@gmail.com>
2017-05-20 20:25:36 -0700

    Add search logic

--- ideas/urls.py
+++ ideas/urls.py
@@ -6,6 +6,7 @@ app_name = 'ideas'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^index/$', views.index, name='index'),
+    url(r'^search-form/$', views.search_form, name='search_form'),
     url(r'^search/$', views.search, name='search'),
     url(r'^(?P<idea_id>[0-9]+)/$', views.detail, name='detail'),
 ]
