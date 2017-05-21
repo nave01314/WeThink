f27fb4cdd4c2b525e4d948ed0636c80b2fbf7e23
Evan Weissburg <nave01314@gmail.com>
2017-05-21 00:49:02 -0700

    Make searching by tags work

--- ideas/models.py
+++ ideas/models.py
@@ -1,17 +1,17 @@
 from django.db import models
 
-class Idea(models.Model):
-    idea_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
-    votes = models.IntegerField(default=0)
-    tags = ["idea"]
-
-    def __str__(self):
-        return self.idea_text
-
 class IdeaTag(models.Model):
     tag_text = models.CharField(max_length=10)
 
     def __str__(self):
         return self.tag_text
+    pass
 
+class Idea(models.Model):
+    idea_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+    votes = models.IntegerField(default=0)
+    tags = models.ManyToManyField(IdeaTag)
+
+    def __str__(self):
+        return self.idea_text
